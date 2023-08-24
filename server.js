5ed3def32610ad24536c4e0559187f783a906ec5
NAYAB SYED DOULATH BASHA <96289292+doulath123@users.noreply.github.com>
2023-08-24 21:02:45 +0530

    updated

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express'); // requre the express framework
+const cors = require("cors");
 var app = express();
 var fs = require('fs'); //require file system object
 
 // Endpoint to Get a list of users
+app.use(cors());
 app.get('/getUsers', function(req, res){
     fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data){
         console.log(data);
@@ -16,3 +18,16 @@ var server = app.listen(8080, function(){
     var port = server.address().port
     console.log("REST API demo app listening at http://%s:%s", host, port)
 })
+
+// const express = require("express");
+// const cors = require("cors");
+// const app = express();
+
+// app.use(cors());
+// app.get("/", (req,res)=>{
+// res.send("server working");
+// });
+
+// app.listen(8080, ()=>{
+// console.log("Server Listen on http://localhost:8080");
+// });
